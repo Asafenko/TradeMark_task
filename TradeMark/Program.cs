e42525adcbddfffbbe494306cc5f58a84c6245fa
Asafenko <asafenko1703@gmail.com>
2023-05-07 20:01:44 +0400

    Initial commit

---
+++ TradeMark/Program.cs
@@ -0,0 +1,25 @@
+﻿using TradeMark;
+
+string[] queries =
+{
+    "Booble",
+    "yyyess",
+    "oooops",
+    "ooooooops",
+    "Boooble",
+    "yyessss",
+    "yyessss",
+    "oooops",
+    "ooooppssss"
+};
+
+Register register = new Register();
+
+foreach(var el in queries )
+{
+    register.Registration(el);
+}
+
+
+register.ListMarks();
+Console.WriteLine(register.Count);
---
+++ TradeMark/Register.cs
@@ -0,0 +1,32 @@
+﻿namespace TradeMark;
+
+public class Register
+{
+    private readonly HashSet<string> _tradeMarks = new ();
+    public int Count => _tradeMarks.Count;
+    public bool Registration(string trademark)
+    {
+        var normalizeTrade = NormalizeTrademark(trademark);
+        return _tradeMarks.Add(normalizeTrade);
+    }
+    
+
+    private static string NormalizeTrademark(string trademark)
+    {
+        var chars = trademark.Where((chr, i) =>
+        {
+            if (i < 2) return true;
+            var prevTwoCharsEqualToCurrent = chr == trademark[i - 1] && chr == trademark[i - 2];
+            return !prevTwoCharsEqualToCurrent;
+        });
+        return new string(chars.ToArray());
+    }
+
+    public void ListMarks()
+    {
+        foreach (var i in _tradeMarks)
+            Console.WriteLine(i);
+         
+    }
+
+}
